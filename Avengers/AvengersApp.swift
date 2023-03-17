99f4dcc20c29c2d12ee1cf6f8f4a1ccfa508a0e9
RedouaneAmourBaracoda <redouane.amour@baracoda.com>
2023-03-17 15:09:31 +0100

    Initial Commit

---
+++ Avengers/AvengersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvengersApp.swift
+//  Avengers
+//
+//  Created by Redouane on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AvengersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
